f33724bfcb72b7bbc28014f4dd5f135c9ea78389
piscoroma <piscoroma@gmail.com>
2017-07-02 01:58:02 +0200

    fixed GUI

--- web_gui/gui_blueprint.py
+++ web_gui/gui_blueprint.py
@@ -3,7 +3,7 @@ from jinja2 import TemplateNotFound
 
 web_gui = Blueprint('web_gui', __name__, url_prefix='/gui', template_folder='templates')
 
-@web_gui.route('/', defaults={'page': 'login'})
+@web_gui.route('/', defaults={'page': 'select'})
 @web_gui.route('/<page>')
 def show(page):
     try:
--- web_gui/static/js/functions.js
+++ web_gui/static/js/functions.js
@@ -1,170 +1,284 @@
 $(document).ready(function() {
 
-    function performLogin() {
-        var username = $("#username").val();
-        var password = $("#password").val();
-        var login_data = {"username":username, "password":password };
+    var username = "admin";
+    var password = "qwerty";
+    var migration_orch_endpoint = "http://127.0.0.1:8083/v1";
+    var login_url = migration_orch_endpoint+ "/login";
+    var graph_url = migration_orch_endpoint+ "/graphs";
+    var state_url = migration_orch_endpoint+ "/status";
+    var tenant_id = "admin";
+    var from_vnf_id = "00000001";
+    var to_vnf_id = "00000002";
+
+    function logOnConsole(message){
+        var now = new Date().toLocaleTimeString();
+        console.log(now + " " + message);
+    }
+
+    function performLogin(user, pass) {
+        var login_data = {"username":user, "password":pass };
         var login_data_json = JSON.stringify(login_data);
+        var deferred = $.Deferred();
         $.ajax({
-            url: "http://127.0.0.1:8083/v1/login",
+            url: login_url,
             type: "POST",
             data: login_data_json,
             contentType: "application/json",
             success: function(resp) {
                 var token = resp.token;
-                console.log("Token: " + token);
+                logOnConsole("[performLogin] Login performed, token: " + token);
                 Cookies.set("token", token);
-                Cookies.set("tenant_id", "admin");
-                return true;
+                logOnConsole("[performLogin] Cookie set 'token': " + Cookies.get('token'));
+                deferred.resolve();
             },
             error: function(err) {
-                alert("Error:" + err.status + " " + err.statusText + " " + err.responseText);
-                return false;
+                deferred.reject("[performLogin] Login error:" + err.status + " " + err.statusText + " " + err.responseText);
             }
         });
+        return deferred.promise();
+    }
+
+
+    function postGraphRequest(token, graph, id_log){
+        logOnConsole("[postGraph] posting graph using token: " + token + "...");
+        $.ajax({
+            url: graph_url,
+            type: "POST",
+            data: JSON.stringify(graph),
+            contentType: "application/json",
+            headers: { 'X-Auth-Token': token },
+            success: function(resp) {
+                var graph_id = resp.id;
+                logOnConsole("[postGraph] posting graph using token: " + token + "...done! Graph_id: " + graph_id);
+                Cookies.set("graph_id", graph_id);
+                logOnConsole("[postGraph] Cookie set 'graph_id': " + Cookies.get('graph_id'));
+                $(id_log).html("Ok!");
+            },
+            error: function(err) {
+                if(err.status===401){
+                    var promise = performLogin(username, password);
+                    promise.done(function(data){
+                        token = Cookies.get("token");
+                        postGraphRequest(token, graph);
+                    });
+                    promise.fail(function (err){
+                        logOnConsole("[postGraph] posting graph using token: " + token + "...failed!");
+                        logOnConsole("Exception: " + err.status + " " + err.statusText + " " + err.responseText);
+                        $(id_log).html("Error!");
+                    });
+                }else{
+                    logOnConsole("[postGraph] posting graph using token: " + token + "...failed!");
+                    logOnConsole("Exception: " + err.status + " " + err.statusText + " " + err.responseText);
+                    $(id_log).html("Error!");
+                }
+            }
+        });
+    }
+
+    function updateGraphRequest(token, graph_id, graph, id_log) {
+        logOnConsole("[updateGraph] updating graph with graph_id: " + graph_id + " using token: " + token + "...");
+        $.ajax({
+            url: graph_url+"/"+graph_id,
+            type: "PUT",
+            data: JSON.stringify(graph),
+            contentType: "application/json",
+            headers: { 'X-Auth-Token': token },
+            success: function(resp) {
+                var new_graph_id = resp.id;
+                logOnConsole("[updateGraph] updating graph with graph_id: " + graph_id + " using token: " + token + "...done! Graph_id: " + new_graph_id);
+                Cookies.set("graph_id", new_graph_id);
+                logOnConsole("[updateGraph] Cookie set 'graph_id': " + Cookies.get('graph_id'))
+                $(id_log).html("Ok!");
+            },
+            error: function(err) {
+                if(err.status===401){
+                    var promise = performLogin(username, password);
+                    promise.done(function(data){
+                        token = Cookies.get("token");
+                        updateGraphRequest(token, graph_id, graph);
+                    });
+                    promise.fail(function (err){
+                        logOnConsole("[updateGraph] updating graph with graph_id: " + graph_id + " using token: " + token + "...failed!");
+                        logOnConsole("Exception:" + err.status + " " + err.statusText + " " + err.responseText);
+                        $(id_log).html("Error!");
+                    });
+                }else {
+                    logOnConsole("[updateGraph] updating graph with graph_id: " + graph_id + " using token: " + token + "...failed!");
+                    logOnConsole("Exception:" + err.status + " " + err.statusText + " " + err.responseText);
+                    $(id_log).html("Error!");
+                }
+            }
+        });
+    }
+
+    function deleteGraphRequest(token, graph_id, id_log) {
+        logOnConsole("[deleteGraph] deleting graph with graph_id: " + graph_id + " using token: " + token + "...");
+        $.ajax({
+            url: graph_url+"/"+graph_id,
+            type: "DELETE",
+            headers: { 'X-Auth-Token': token },
+            success: function(resp) {
+                logOnConsole("[deleteGraph] deleting graph with graph_id: " + graph_id + " using token: " + token + "...done! Graph_id: " + graph_id);
+                logOnConsole("[deleteGraph] Cookie 'graph_id': " + Cookies.get('graph_id') + "removed");
+                Cookies.remove("graph_id");
+                $(id_log).html("Ok!");
+            },
+            error: function(err) {
+                if(err.status===401){
+                    var promise = performLogin(username, password);
+                    promise.done(function(data){
+                        token = Cookies.get("token");
+                        deleteGraphRequest(token, graph_id);
+                    });
+                    promise.fail(function (err){
+                        logOnConsole("[deleteGraph] deleting graph with graph_id: " + graph_id + " using token: " + token + "...failed!");
+                        logOnConsole("Error:" + err.status + " " + err.statusText + " " + err.responseText);
+                        $(id_log).html("Error!");
+                    });
+                }else {
+                    logOnConsole("[deleteGraph] deleting graph with graph_id: " + graph_id + " using token: " + token + "...failed!");
+                    logOnConsole("Error:" + err.status + " " + err.statusText + " " + err.responseText);
+                    $(id_log).html("Error!");
+                }
+            }
+        });
+    }
+
+    function getStateFromVnf(tenant_id, graph_id, vnf_id){
+        var deferred = $.Deferred();
+        logOnConsole("[getStateFromVnf] "+ tenant_id +"/" + graph_id + "/" + vnf_id);
+        $.ajax({
+            url: state_url,
+            type: "GET",
+            headers: { 'tenant_id': tenant_id, 'graph_id': graph_id, 'vnf_id': vnf_id},
+            contentType: "application/json",
+            cache: false,
+            success: function(resp) {
+                deferred.resolve(resp);
+            },
+            error: function(err) {
+                deferred.reject("[getStateFromVnf] Error:" + err.status + " " + err.statusText + " " + err.responseText);
+            }
+        });
+        return deferred.promise();
+    }
+
+    function pushStateIntoVnf(tenant_id, graph_id, vnf_id, status){
+        var deferred = $.Deferred();
+        logOnConsole("[pushStateIntoVnf] "+ tenant_id +"/" + graph_id + "/" + vnf_id);
+        $.ajax({
+            url: state_url,
+            type: "PUT",
+            data: status,
+            contentType: "application/json",
+            headers: { 'tenant_id': tenant_id, 'graph_id': graph_id, 'vnf_id': vnf_id},
+            cache: false,
+            success: function(resp) {
+                deferred.resolve();
+            },
+            error: function(err) {
+                deferred.reject("[pushStateIntoVnf] Error:" + err.status + " " + err.statusText + " " + err.responseText);
+            }
+        });
+        return deferred.promise();
     }
 
 
     function postGraph(params) {
         var token = Cookies.get("token");
-        console.log("Token: " + token);
-        $.ajax({
-            url: "http://127.0.0.1:8083/v1/graphs",
-            type: "POST",
-            data: JSON.stringify(params.data.graph),
-            contentType: "application/json",
-            headers: { 'X-Auth-Token': token },
-            success: function(resp) {
-                var graph_id = resp.id;
-                console.log("graph_id: " + graph_id);
-                Cookies.set("graph_id", graph_id);
-                return true;
-            },
-            error: function(err) {
-                alert("Error:" + err.status + " " + err.statusText + " " + err.responseText);
-                return false;
-            }
-        });
+        if (token === undefined){
+            logOnConsole("[postGraph] token is null, so perform the login...");
+            var promise = performLogin(username, password);
+            promise.done(function(data){
+                    token = Cookies.get("token");
+                    logOnConsole("[postGraph] token is null, so perform the login...done! Token: " + token);
+                    postGraphRequest(token, params.data.graph, params.data.id_log);
+            });
+            promise.fail(function (err){
+                logOnConsole("[postGraph] token is null, so perform the login...failed!");
+                logOnConsole("Exception: " + err);
+            });
+        }else{
+            postGraphRequest(token, params.data.graph, params.data.id_log);
+        }
     }
 
-
     function updateGraph(params) {
+        var graph_id = Cookies.get("graph_id");
         var token = Cookies.get("token");
-        console.log("Token: " + token);
-        $.ajax({
-            url: "http://127.0.0.1:8083/v1/graphs/"+params.data.graph_id,
-            type: "PUT",
-            data: JSON.stringify(params.data.graph),
-            contentType: "application/json",
-            headers: { 'X-Auth-Token': token },
-            success: function(resp) {
-                var graph_id = resp.id;
-                console.log("graph_id: " + graph_id);
-                Cookies.set("graph_id", graph_id);
-                return true;
-            },
-            error: function(err) {
-                alert("Error:" + err.status + " " + err.statusText + " " + err.responseText);
-                return false;
-            }
-        });
+        if (token === undefined){
+            logOnConsole("[updateGraph] token is null, so perform the login...");
+            var promise = performLogin(username, password);
+            promise.done(function(data){
+                    token = Cookies.get("token");
+                    logOnConsole("[updateGraph] token is null, so perform the login...done! Token: " + token);
+                    updateGraphRequest(token, graph_id, params.data.graph, params.data.id_log);
+            });
+            promise.fail(function (err){
+                logOnConsole("[updateGraph] token is null, so perform the login...failed!");
+                logOnConsole("Exception: " + err);
+            });
+        }else{
+            updateGraphRequest(token, graph_id, params.data.graph, params.data.id_log);
+        }
     }
 
     function deleteGraph(params) {
+        var graph_id = Cookies.get("graph_id");
         var token = Cookies.get("token");
-        console.log("Token: " + token);
-        console.log("graph_id: " + params.data.graph_id);
-        $.ajax({
-            url: "http://127.0.0.1:8083/v1/graphs/"+params.data.graph_id,
-            type: "DELETE",
-            headers: { 'X-Auth-Token': token },
-            success: function(resp) {
-                console.log("delete success");
-                Cookies.remove("graph_id");
-                Cookies.remove("tenant_id");
-                return true;
-            },
-            error: function(err) {
-                alert("Error:" + err.status + " " + err.statusText + " " + err.responseText);
-                return false;
-            }
-        });
+        if (token === undefined){
+            logOnConsole("[deleteGraph] token is null, so perform the login...");
+            var promise = performLogin(username, password);
+            promise.done(function(data){
+                    token = Cookies.get("token");
+                    logOnConsole("[deleteGraph] token is null, so perform the login...done! Token: " + token);
+                    deleteGraphRequest(token, graph_id, params.data.id_log);
+            });
+            promise.fail(function (err){
+                logOnConsole("[deleteGraph] token is null, so perform the login...failed!");
+                logOnConsole("Exception: " + err);
+            });
+        }else{
+            deleteGraphRequest(token, graph_id, params.data.id_log);
+        }
     }
 
     function migrateState(params) {
-        var tenant_id = params.data.tenant_id;
-        var graph_id = params.data.graph_id;
-        var from_vnf_id = params.data.from_vnf_id;
-        var to_vnf_id = params.data.to_vnf_id;
-        $.when(getStateFromVnf(tenant_id, graph_id, from_vnf_id))
-            .then(function (status) {
-                pushStateIntoVnf(tenant_id, graph_id, to_vnf_id, status);
-            })
-            .fail(function(err){
-                console.log(err)
+        var graph_id = Cookies.get("graph_id");
+        logOnConsole("[migrateState] "+ tenant_id +"/" + graph_id + "/" + from_vnf_id + "/" + to_vnf_id);
+        logOnConsole("[migrateState] Migration started...");
+        var promise = getStateFromVnf(tenant_id, graph_id, from_vnf_id);
+            promise.done(function(data){
+                logOnConsole("[migrateState] getStateFromVnf success!");
+                status = JSON.stringify(data);
+                promise = pushStateIntoVnf(tenant_id, graph_id, to_vnf_id, status);
+                promise.done(function(data){
+                    logOnConsole("[migrateState] pushStateIntoVnf success!");
+                    logOnConsole("[migrateState] Migration completed!");
+                    $(params.data.id_log).html("Ok!");
+                });
+                promise.fail(function(err){
+                    logOnConsole("[migrateState] pushStateIntoVnf failure!");
+                    logOnConsole("Exception: " + err);
+                    logOnConsole("[migrateState] Migration failed!");
+                    $(params.data.id_log).html("Error!");
+                });
             });
-
-        /*
-         var status = getStateFromVnf(tenant_id, graph_id, from_vnf_id);
-         console.log("status from var: " + status);
-         if(status!== null)
-         pushStateIntoVnf(tenant_id, graph_id, to_vnf_id, status);
-         */
+            promise.fail(function(err){
+                logOnConsole("[migrateState] getStateFromVnf failure!");
+                logOnConsole("Exception: " + err);
+                logOnConsole("[migrateState] Migration failed!");
+                $(params.data.id_log).html("Error!");
+            });
     }
 
-
-    function getStateFromVnf(tenant_id, graph_id, vnf_id){
-        console.log("tenant_id: " + tenant_id);
-        console.log("graph_id: " + graph_id);
-        console.log("vnf_id: " + vnf_id);
-        var retVal = null;
-        $.ajax({
-            url: "http://127.0.0.1:8083/v1/status",
-            type: "GET",
-            headers: { 'tenant_id': tenant_id, 'graph_id': graph_id, 'vnf_id': vnf_id},
-            cache: false,
-            success: function(resp) {
-                console.log("getCurrentStateFromVnf success");
-                console.log("status get: " + JSON.stringify(resp));
-                retVal = JSON.stringify(resp);
-            },
-            error: function(err) {
-                console.log("getCurrentStateFromVnf failure");
-                alert("Error:" + err.status + " " + err.statusText + " " + err.responseText);
-            }
-        });
-        return retVal;
-    }
-
-    function pushStateIntoVnf(tenant_id, graph_id, vnf_id, status){
-        console.log("status to push: " + status);
-        console.log("tenant_id: " + tenant_id);
-        console.log("graph_id: " + graph_id);
-        console.log("vnf_id: " + vnf_id);
-        $.ajax({
-            url: "http://127.0.0.1:8083/v1/status",
-            type: "PUT",
-            data: status,
-            cache: false,
-            headers: { 'tenant_id': tenant_id, 'graph_id': graph_id, 'vnf_id': vnf_id},
-            success: function(resp) {
-                console.log("pushStatusIntoVnf success");
-                return true
-            },
-            error: function(err) {
-                console.log("pushStatusIntoVnf failure");
-                alert("Error:" + err.status + " " + err.statusText + " " + err.responseText);
-                return false;
-            }
-        });
-    }
 
     $("#form-login").submit(performLogin);
-    $("#btn-deploy_initial_graph").click({ graph: graph1 }, postGraph);
-    $("#btn-deploy_second_nat").click({ graph_id: Cookies.get('graph_id'), graph: graph2 }, updateGraph);
-    $("#btn-migrate_state").click({tenant_id: Cookies.get('tenant_id'), graph_id: Cookies.get('graph_id'), from_vnf_id: "00000001", to_vnf_id: "00000002" }, migrateState);
-    $("#btn-delete_old_nat").click({ graph_id: Cookies.get('graph_id'), graph: graph3 }, updateGraph);
-    $("#btn-undeploy_everything").click({ graph_id: Cookies.get('graph_id') }, deleteGraph);
+    $("#btn-deploy_initial_graph").click({ graph: graph1, id_log: ".log-deploy_initial_graph" }, postGraph);
+    $("#btn-deploy_second_nat").click({ graph: graph2, id_log: ".log-deploy_second_nat" }, updateGraph);
+    $("#btn-migrate_state").click({id_log: ".log-migrate_state"}, migrateState);
+    $("#btn-delete_old_nat").click({ graph: graph3, id_log: ".log-delete_old_nat" }, updateGraph);
+    $("#btn-undeploy_everything").click({id_log: ".log-undeploy_everything"}, deleteGraph);
 
 
 });
